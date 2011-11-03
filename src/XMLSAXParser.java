2458023b0c358bc3d5b6b758a8e03736eee9fbca
TheNeisius <abrageddon@gmail.com>
2011-11-03 10:44:08 -0700

    reads author mapping; needs page reading function

--- src/XMLSAXParser.java
+++ src/XMLSAXParser.java
@@ -97,6 +97,24 @@ public class XMLSAXParser extends DefaultHandler {
                     Statement st = connection.createStatement();
                     st.executeUpdate("INSERT INTO tbl_dblp_document (" + tempDoc.getColumns() + ") VALUES (" + tempDoc.getValues() + ")");
 
+                    //TODO INSERT Author IDs into tbl_author_document_mapping
+
+                    int docID = 0;
+                    if (tempDoc.getAuthorsIDs().size() > 0){
+                        st = connection.createStatement();
+                        //TODO find a better way to get last added document
+                        //largest id should be newest, unless someone manually input an id
+                        ResultSet docIDQ = st.executeQuery("SELECT max(id) FROM tbl_dblp_document");
+                        if(docIDQ.next()){
+                            docID = docIDQ.getInt(1);
+                        }
+                    }
+                    for (Integer author: tempDoc.getAuthorsIDs()){
+                        st = connection.createStatement();
+                        st.executeUpdate("INSERT INTO tbl_author_document_mapping (doc_id, author_id) VALUES ('"+docID+"','" + author + "')");
+                        //TODO create one multi statement
+                    }
+
                 } catch (SQLException ex) {
                     System.out.println(ex.getMessage());
                 }
